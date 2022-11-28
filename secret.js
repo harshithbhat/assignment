dd9f544277c57efeb6a6c539038157f996447608
harshithabhat <harshitha.bhat@niveussolutions.com>
2022-11-28 17:51:03 +0530

    new changes Changes

--- secret.js
+++ secret.js
@@ -1,3 +1,6 @@
-const url = 'mongodb+srv://harshitha:harshitha@cluster0.2stoscx.mongodb.net/?retryWrites=true&w=majority'
 
+require('dotenv').config()
+const url = process.env.MONGODB_URL
 module.exports = url
+
+// asia-south1-docker.pkg.dev/niveustraining/test/nodejsassignment@sha256:46bf26e016083f526d840a0612c58c31664854ed351b5b33b9586865441dd81e
