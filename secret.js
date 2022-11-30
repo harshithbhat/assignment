08b6c437a58aa418f716186f5d52b240856c8221
harshithabhat <harshitha.bhat@niveussolutions.com>
2022-11-30 20:03:26 +0530

    New changes

--- secret.js
+++ secret.js
@@ -2,5 +2,3 @@
 require('dotenv').config()
 const url = process.env.MONGODB_URL
 module.exports = url
-
-// asia-south1-docker.pkg.dev/niveustraining/test/nodejsassignment@sha256:46bf26e016083f526d840a0612c58c31664854ed351b5b33b9586865441dd81e
